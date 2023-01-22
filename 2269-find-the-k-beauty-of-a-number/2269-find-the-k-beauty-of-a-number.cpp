cdd742b7409a6e086d6f142bbe6f0f8fa30ad8d9
Elmo Lin <75987103+Elmo-Lin@users.noreply.github.com>
2023-01-22 17:46:11 +0800

    Time: 6 ms (5.02%), Space: 5.9 MB (92.89%) - LeetHub

--- 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.cpp
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.cpp
@@ -3,7 +3,6 @@ public:
     int divisorSubstrings(int num, int k) {
         int res=0;
         string s=to_string(num);
-        
         for(int i=0; i<=s.size()-k; i++){
             int t=stoi(s.substr(i, k));
             if(t!=0 && num%t==0){
