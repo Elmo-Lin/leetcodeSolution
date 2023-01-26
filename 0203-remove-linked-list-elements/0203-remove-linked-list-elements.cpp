e4bd80d11fcf6be39896058f0615385aa9df5635
Elmo Lin <75987103+Elmo-Lin@users.noreply.github.com>
2023-01-26 12:27:30 +0800

    Time: 31 ms (40.53%), Space: 15.2 MB (20.90%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
@@ -15,7 +15,9 @@ public:
         ListNode* curr=&dummy;
         while(curr && curr->next){
             if(curr->next->val==val){
+                ListNode* temp=curr->next;
                 curr->next=curr->next->next;
+                delete temp;
             }
             else{
                 curr=curr->next;
