e3427c91c8c7edad4029f0c45c19e6d3e46b0440
Elmo Lin <75987103+Elmo-Lin@users.noreply.github.com>
2023-01-27 12:42:19 +0800

    Time: 0 ms (100.00%), Space: 5.9 MB (65.51%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -4,12 +4,14 @@ public:
         if(n<=1){
             return n;
         }
-        vector<int> dp(n+1, 0);
-        dp[0]=0;
-        dp[1]=1;
+        int sum=0;
+        int a=0;
+        int b=1;
         for(int i=2; i<=n; i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            sum=a+b;
+            a=b;
+            b=sum;
         }
-        return dp[n];
+        return sum;
     }
 };
