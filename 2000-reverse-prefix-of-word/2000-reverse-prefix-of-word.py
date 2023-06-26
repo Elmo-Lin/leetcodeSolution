9003dfad4fa24fed86dd1c1d63f9aaaa24d2727f
Elmo Lin <75987103+Elmo-Lin@users.noreply.github.com>
2023-06-26 17:38:03 +0800

    Time: 57 ms (8.15%), Space: 16.3 MB (68.72%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
@@ -0,0 +1,6 @@
+class Solution:
+    def reversePrefix(self, word: str, ch: str) -> str:
+        if ch not in word:
+            return word
+        index=word.index(ch)
+        return word[index::-1]+word[index+1:]
