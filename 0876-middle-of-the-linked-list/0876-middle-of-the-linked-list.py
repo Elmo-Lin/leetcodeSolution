5e882d50ce7d8add06b07eb46883d450716f6c2f
Elmo Lin <75987103+Elmo-Lin@users.noreply.github.com>
2023-06-27 09:52:09 +0800

    Time: 30 ms (99.58%), Space: 16.4 MB (29.24%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow=head
+        while head and head.next:
+            slow=slow.next
+            head=head.next.next
+        
+        return slow
