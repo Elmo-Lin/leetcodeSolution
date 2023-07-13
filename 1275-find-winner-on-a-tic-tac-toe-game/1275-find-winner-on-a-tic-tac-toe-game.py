00024fdcb2c9019b97bb5ec72bca91db3a888136
Elmo Lin <75987103+Elmo-Lin@users.noreply.github.com>
2023-07-13 15:43:12 +0800

    Time: 54 ms (28.14%), Space: 16.4 MB (54.10%) - LeetHub

--- 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.py
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.py
@@ -1,15 +1,15 @@
 class Solution:
     def tictactoe(self, moves: List[List[int]]) -> str:
-        win = [
-            [[0,0],[0,1],[0,2]], [[0,0],[1,0],[2,0]],
-            [[1,0],[1,1],[1,2]], [[0,1],[1,1],[2,1]],
-            [[2,0],[2,1],[2,2]], [[0,2],[1,2],[2,2]],
-            [[0,0],[1,1],[2,2]], [[0,2],[1,1],[2,0]]
+        win=[
+            [[0, 0], [0, 1], [0, 2]], [[0, 0], [1, 0], [2, 0]],
+            [[1, 0], [1, 1], [1, 2]], [[0, 1], [1, 1], [2, 1]],
+            [[2, 0], [2, 1], [2, 2]], [[0, 2], [1, 2], [2, 2]],
+            [[0, 0], [1, 1], [2, 2]], [[0, 2], [1, 1], [2, 0]]
         ]
         
         # Distribute
-        A = [list(step) for step in moves[::2]]
-        B = [list(step) for step in moves[1::2]]
+        A=[list(step) for step in moves[::2]]
+        B=[list(step) for step in moves[1::2]]
             
         # Check winner
         for win_case in win:
@@ -19,7 +19,7 @@ class Solution:
                 return "B"
                
         # No winner
-        if len(moves) == 9: 
+        if len(moves)==9: 
             return "Draw"
         else: 
             return "Pending"
