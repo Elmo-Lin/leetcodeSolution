5f755bf12de26c818f43a60906a69076b068824c
Elmo Lin <75987103+Elmo-Lin@users.noreply.github.com>
2023-08-07 21:24:28 +0800

    Time: 70 ms (85.30%), Space: 17 MB (86.49%) - LeetHub

--- 1529-minimum-suffix-flips/1529-minimum-suffix-flips.py
+++ 1529-minimum-suffix-flips/1529-minimum-suffix-flips.py
@@ -2,7 +2,7 @@ class Solution:
     def minFlips(self, target: str) -> int:
         res=0
         for i in range(0, len(target)-1):
-            if target[i+1]!=target[i]:
+            if target[i]!=target[i+1]:
                 res+=1
         
         return res+(target[0]=='1')
