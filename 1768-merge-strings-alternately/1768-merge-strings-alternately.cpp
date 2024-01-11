d90311cc769bdc57605443503e4195d37e8787a9
Elmo Lin <75987103+Elmo-Lin@users.noreply.github.com>
2024-01-12 00:48:45 +0800

    Time: 3 ms (49.46%), Space: 6.6 MB (77.46%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string res;
+        int i=0;
+        int j=0;
+        while(i<word1.size() && j<word2.size()){
+            res+=word1[i];
+            i++;
+            res+=word2[j];
+            j++;
+            
+        }
+        while(i<word1.size()){
+            res+=word1[i];
+            i++;
+        }
+        while(j<word2.size()){
+            res+=word2[j];
+            j++;
+        }
+        
+        return res;
+    }
+};
