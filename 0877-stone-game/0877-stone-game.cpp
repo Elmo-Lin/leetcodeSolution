eadbacb1709baff05c1d151903eb07b33a6d18c0
Elmo Lin <75987103+Elmo-Lin@users.noreply.github.com>
2024-01-26 21:26:17 +0800

    Time: 7 ms (46.97%), Space: 18.3 MB (13.24%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n = piles.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        for(int i=0; i<n; i++){
+            dp[i][i]=piles[i];
+        }
+
+        for(int length=2; length<=n; length++){
+            for(int i=0; i<n-length+1; i++){
+                int j=i+length-1;
+                dp[i][j]=max(piles[i]-dp[i+1][j], piles[j]-dp[i][j-1]);
+            }
+        }
+
+        return dp[0][n-1]>0;
     }
 };
