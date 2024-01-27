e4bb9fa7e501c8527833e9d131636b263824b6c7
Elmo Lin <75987103+Elmo-Lin@users.noreply.github.com>
2024-01-27 12:21:19 +0800

    Time: 20 ms (27.98%), Space: 15.7 MB (18.55%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n=s.size();
+        set<string> dict(wordDict.begin(), wordDict.end());
+        vector<bool> dp(n+1, false);
+        dp[0]=true;
+        
+        for(int i=1; i<=n; i++){
+            for(int j=0; j<i; j++){
+                if(dp[j] && dict.find(s.substr(j, i-j))!=dict.end()){
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+        
+        return dp[n];
+    }
+};
