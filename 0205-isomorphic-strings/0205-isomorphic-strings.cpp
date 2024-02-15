32d4608bf1505f4e62f004335656c8e0f71834bb
Elmo Lin <75987103+Elmo-Lin@users.noreply.github.com>
2024-02-16 00:18:39 +0800

    Time: 7 ms (51.43%), Space: 9.3 MB (21.83%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        map<char, int> m;
+        map<char, int> n;
+        for(int i=0; i<s.size(); i++){
+            if(m[s[i]]!=n[t[i]]){
+                return false;
+            }
+            m[s[i]]=i+1;
+            n[t[i]]=i+1;
+        }
+        return true;
+    }
+};
