8ae6473bf57e0deab1a6f50c719b6c35379d74d8
Elmo Lin <75987103+Elmo-Lin@users.noreply.github.com>
2024-03-14 14:03:33 +0800

    Time: 0 ms (100.00%), Space: 7.8 MB (61.44%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        unordered_map<char, int> m1;
+        unordered_map<string, int> m2;
+        istringstream in(s);
+        int i=0;
+        int n=pattern.size();
+        for(string word; in>>word; i++){
+            if(i==n||m1[pattern[i]]!=m2[word]){
+                return false;
+            }
+            m1[pattern[i]]=m2[word]=i+1;
+        }
+        return i==n;
+    }
+};
