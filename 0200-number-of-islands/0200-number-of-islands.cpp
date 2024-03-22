3f529b77db02a96ef5869c453cf409bd9201a91e
Elmo Lin <75987103+Elmo-Lin@users.noreply.github.com>
2024-03-22 21:42:04 +0800

    Time: 28 ms (62.43%), Space: 15.6 MB (94.13%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
+        int res=0;
         int m=grid.size();
         int n=grid[0].size();
-        int res=0;
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(grid[i][j]=='1'){
                     res++;
-                    helper(grid, i, j);
+                    helper(grid, res, i, j);
                 }
             }
         }
         return res;
     }
-    void helper(vector<vector<char>>& grid, int i, int j){
+    void helper(vector<vector<char>>& grid, int& res, int i, int j){
         int m=grid.size();
         int n=grid[0].size();
-        if(i<0 || j<0 || i>=m || j>=n || grid[i][j]!='1'){
+        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]=='0'){
             return;
         }
         grid[i][j]='0';
-        helper(grid, i+1, j);
-        helper(grid, i, j+1);
-        helper(grid, i-1, j);
-        helper(grid, i, j-1);
+        helper(grid, res, i+1, j);
+        helper(grid, res, i, j+1);
+        helper(grid, res, i-1, j);
+        helper(grid, res, i, j-1);
     }
 };
