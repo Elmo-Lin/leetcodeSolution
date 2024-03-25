c9156b473d5cac176890ea0cedb96fec2787ad0a
Elmo Lin <75987103+Elmo-Lin@users.noreply.github.com>
2024-03-25 13:32:48 +0800

    Time: 41 ms (75.61%), Space: 50.8 MB (82.56%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        int reach=0;
+        for(int i=0; i<nums.size(); i++){
+            if(i>reach || reach>=n-1){
+                cout<<i;
+                break;
+            }
+            reach=max(reach, i+nums[i]);
+        }
+        return reach>=n-1;
+    }
+};
