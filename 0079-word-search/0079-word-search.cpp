e657124196ad866139dfc2eb1a2d972ceddb976b
Elmo Lin <75987103+Elmo-Lin@users.noreply.github.com>
2024-03-28 17:30:03 +0800

    Time: 1263 ms (8.73%), Space: 11.8 MB (18.85%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        if(board.empty() || board[0].empty()){
+            return false;
+        }
+        int m=board.size();
+        int n=board[0].size();
+        vector<vector<bool>> visited(m, vector<bool>(n));
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(search(board, word, 0, i, j, visited)){
+                    return true;
+                }        
+            }
+        }
+        return false;
+    }
+    bool search(vector<vector<char>>& board, string word, int idx, int i, int j, vector<vector<bool>>& visited){
+        if(idx==word.size()){
+            return true;
+        }
+        int m=board.size();
+        int n=board[0].size();
+        if(i<0 || i>=m || j<0 || j>=n || visited[i][j] || word[idx]!=board[i][j]){
+            return false;
+        }
+        visited[i][j]=true;
+        bool res=search(board, word, idx+1, i+1, j, visited) ||
+                 search(board, word, idx+1, i, j+1, visited) ||
+                 search(board, word, idx+1, i-1, j, visited) ||
+                 search(board, word, idx+1, i, j-1, visited);
+        visited[i][j]=false;
+        return res;
+    }
+};
