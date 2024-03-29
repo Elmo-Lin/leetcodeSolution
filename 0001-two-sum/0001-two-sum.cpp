9474cfc67d09c579f895da8d3aa82abdd632117b
Elmo Lin <75987103+Elmo-Lin@users.noreply.github.com>
2024-03-29 18:09:33 +0800

    Time: 6 ms (89.54%), Space: 14.1 MB (29.38%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,12 +3,10 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> m;
         for(int i=0; i<nums.size(); i++){
-            m[nums[i]]=i;
-        }
-        for(int i=0; i<nums.size(); i++){
-            if(m.count(target-nums[i]) && i!=m[target-nums[i]]){
-                return {i, m[target-nums[i]]};
+            if(m.count(target-nums[i])){
+                return {m[target-nums[i]], i};
             }
+            m[nums[i]]=i;
         }
         return {};
     }
