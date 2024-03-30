4739b235a4fd5e13a8dc6d68cded5d9b34fa8da5
Elmo Lin <75987103+Elmo-Lin@users.noreply.github.com>
2024-03-30 20:49:56 +0800

    Time: 12 ms (41.78%), Space: 9.3 MB (43.32%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int res=0;
+        int maxCount=0;
+        int start=0;
+        unordered_map<char, int> m;
+        for(int i=0; i<s.size(); i++){
+            m[s[i]]++;
+            maxCount=max(maxCount, m[s[i]]);
+            while(i-start+1-maxCount>k){
+                m[s[start]]--;
+                start++;
+            }
+            res=max(res, i-start+1);
+        }
+        return res;
+    }
+};
