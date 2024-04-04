564c2fe9c7811d9b00cfc5820fd79e6c5ce1f1d2
Elmo Lin <75987103+Elmo-Lin@users.noreply.github.com>
2024-04-04 22:30:11 +0800

    Time: 1249 ms (9.36%), Space: 11.8 MB (18.36%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -9,27 +9,27 @@ public:
         vector<vector<bool>> visited(m, vector<bool>(n));
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(search(board, word, 0, i, j, visited)){
+                if(dfs(board, word, 0, i, j, visited)){
                     return true;
                 }
             }
         }
         return false;
     }
-    bool search(vector<vector<char>>& board, string word, int idx, int i, int j, vector<vector<bool>>& visited){
+    bool dfs(vector<vector<char>>& board, string word, int idx, int i, int j, vector<vector<bool>>& visited){
         if(idx==word.size()){
             return true;
         }
         int m=board.size();
         int n=board[0].size();
-        if(i<0 || i>=m || j<0 || j>=n || visited[i][j]==true || word[idx]!=board[i][j]){
+        if(i<0 || i>=m || j<0 || j>=n || visited[i][j] || board[i][j]!=word[idx]){
             return false;
         }
         visited[i][j]=true;
-        bool res=search(board, word, idx+1, i+1, j, visited) ||
-                 search(board, word, idx+1, i, j+1, visited) ||
-                 search(board, word, idx+1, i-1, j, visited) ||
-                 search(board, word, idx+1, i, j-1, visited);
+        bool res=dfs(board, word, idx+1, i+1, j, visited) || 
+                 dfs(board, word, idx+1, i, j+1, visited) || 
+                 dfs(board, word, idx+1, i-1, j, visited) || 
+                 dfs(board, word, idx+1, i, j-1, visited);
         visited[i][j]=false;
         return res;
     }
