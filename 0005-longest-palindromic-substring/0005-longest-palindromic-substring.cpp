57e4d443e004090ed13856b7d6b743219ee359ce
Elmo Lin <75987103+Elmo-Lin@users.noreply.github.com>
2024-04-05 22:59:34 +0800

    Time: 444 ms (20.09%), Space: 25.4 MB (34.18%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        if(s.size()==1){
-            return s;
+        int n=s.size();
+        vector<vector<bool>> dp(n, vector<bool>(n));
+        int left=0;
+        int maxLen=1;
+        for(int j=0; j<n; j++){
+            dp[j][j]=true;
+            for(int i=0; i<j; i++){
+                dp[i][j]=(s[i]==s[j] && (j-i==1 || dp[i+1][j-1]));
+                if(dp[i][j] && maxLen<j-i+1){
+                    maxLen=j-i+1;
+                    left=i;
+                }
+            }
         }
-        
-        int start=0;
-        int maxLen=0;
-        for(int i=0; i<s.size()-1; i++){
-            searchPalindrome(s, i, i, start, maxLen);
-            searchPalindrome(s, i, i+1, start, maxLen);
-        }
-        return s.substr(start, maxLen);
-    }
-    void searchPalindrome(string s, int left, int right, int& start, int& maxLen){
-        while(left>=0 && right<s.size() && s[left]==s[right]){
-            left--;
-            right++;
-        }
-        if(maxLen<right-left-1){
-            maxLen=right-left-1;
-            start=left+1;
-        }
+        return s.substr(left, maxLen);
     }
 };
