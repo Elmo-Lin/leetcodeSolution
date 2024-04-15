6fbdc29e09a55e1ca2657245b7c1e43a262465b0
Elmo Lin <75987103+Elmo-Lin@users.noreply.github.com>
2024-04-15 11:00:52 +0800

    Time: 8 ms (7.69%), Space: 11.3 MB (64.05%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -23,16 +23,16 @@ class Solution {
 public:
     Node* cloneGraph(Node* node) {
         if(!node){
-            return NULL;
+            return nullptr;
         }
         unordered_map<Node*, Node*> m;
         queue<Node*> q{{node}};
-        Node *clone=new Node(node->val);
+        Node* clone=new Node(node->val);
         m[node]=clone;
         while(!q.empty()){
-            Node *t=q.front();
+            Node* t=q.front();
             q.pop();
-            for(Node *neighbor : t->neighbors){
+            for(Node* neighbor : t->neighbors){
                 if(!m.count(neighbor)){
                     m[neighbor]=new Node(neighbor->val);
                     q.push(neighbor);
