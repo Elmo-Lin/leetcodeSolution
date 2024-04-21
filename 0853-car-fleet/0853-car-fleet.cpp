5a5cb00d8e12606a3c3825a8ed9684c238235d67
Elmo Lin <75987103+Elmo-Lin@users.noreply.github.com>
2024-04-21 12:39:11 +0800

    Time: 158 ms (27.87%), Space: 82.1 MB (62.50%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.cpp
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
         int res=0;
-        double cur=0;
+        int n=position.size();
         priority_queue<pair<int, int>> pq;
-        for(int i=0; i<position.size(); i++){
+        for(int i=0; i<n; i++){
             pq.push({position[i], speed[i]});
         }
+        double cur=0;
         while(!pq.empty()){
             auto t=pq.top();
             pq.pop();
@@ -20,3 +21,11 @@ public:
         return res;
     }
 };
+
+
+// position: 10, 8, 0, 5, 3
+
+//  distance: 2, 4, 12, 7, 9
+
+//     speed: 2, 4, 1,  1, 3
+//      time: 1, 1, 12, 7, 3
