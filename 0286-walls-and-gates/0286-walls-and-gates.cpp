254c9410b6a27a4daf4392448788b8b7042abc4b
Elmo Lin <75987103+Elmo-Lin@users.noreply.github.com>
2024-04-29 12:59:02 +0800

    Time: 45 ms (27.12%), Space: 23.7 MB (30.16%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.cpp
+++ 0286-walls-and-gates/0286-walls-and-gates.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     void wallsAndGates(vector<vector<int>>& rooms) {
-        queue<pair<int, int>> q;
-        vector<vector<int>> dirs{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        queue<vector<int>> q;
+        vector<vector<int>> dirs={{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         int m=rooms.size();
         int n=rooms[0].size();
         for(int i=0; i<m; i++){
@@ -13,17 +13,19 @@ public:
             }
         }
         while(!q.empty()){
-            int i=q.front().first;
-            int j=q.front().second;
-            q.pop();
-            for(int k=0; k<4; k++){
-                int x=i+dirs[k][0];
-                int y=j+dirs[k][1];
-                if(x<0 || x>=m || y<0 || y>=n || rooms[x][y]!=INT_MAX){
-                    continue;
+            int size=q.size();
+            for(int i=0; i<size; i++){
+                auto cur=q.front();
+                q.pop();
+                for(int k=0; k<4; k++){
+                    int x=cur[0]+dirs[k][0];
+                    int y=cur[1]+dirs[k][1];
+                    if(x<0 || x>=m || y<0 || y>=n || rooms[x][y]!=INT_MAX){
+                        continue;
+                    }
+                    rooms[x][y]=rooms[cur[0]][cur[1]]+1;
+                    q.push({x, y});
                 }
-                rooms[x][y]=rooms[i][j]+1;
-                q.push({x, y});
             }
         }
     }
