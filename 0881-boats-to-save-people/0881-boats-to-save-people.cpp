72338ce7f959c13f3479ce0ccb7c15b58e8cdc1b
Elmo Lin <75987103+Elmo-Lin@users.noreply.github.com>
2024-05-04 09:32:39 +0800

    Time: 67 ms (10.92%), Space: 45.5 MB (8.97%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int count=0;
+        int left=0;
+        int right=people.size()-1;
+        while(left<=right){
+            if(people[left]+people[right]<=limit){
+                left++;
+            }
+            right--;
+            count++;
+        }
+        return count;
+    }
+};
