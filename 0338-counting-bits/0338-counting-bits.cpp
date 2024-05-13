e7c5c4760b9e7a5b1d1a2ab8c2a542f2b2812cdd
Elmo Lin <75987103+Elmo-Lin@users.noreply.github.com>
2024-05-13 10:44:37 +0800

    Time: 7 ms (18.73%), Space: 9.6 MB (25.31%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -3,13 +3,13 @@ public:
     vector<int> countBits(int n) {
         vector<int> res;
         for(int i=0; i<=n; i++){
+            int num=i;
             int count=0;
-            int temp=i;
-            while(temp!=0){
-                if(temp%2==1){
+            while(num){
+                if(num%2==1){
                     count++;
                 }
-                temp/=2;
+                num/=2;
             }
             res.push_back(count);
         }
