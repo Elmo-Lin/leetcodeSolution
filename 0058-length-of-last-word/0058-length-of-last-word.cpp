fc0b294ca98573c254b8f318ad47ed656677748f
Elmo Lin <75987103+Elmo-Lin@users.noreply.github.com>
2024-09-03 23:16:28 +0800

    Time: 0 ms (100.00%), Space: 7.9 MB (60.51%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int res=0;
+        int count=0;
         bool found=false;
         for(int i=s.size()-1; i>=0; i--){
             if(s[i]!=' '){
-                res++;
                 found=true;
+                count++;
             }else if(found){
                 break;
             }
         }
-        return res;
+        return count;
     }
 };
