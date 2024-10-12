3f0328d2ab99b5a62a21ec6b607c66a203b24fc1
Elmo Lin <75987103+Elmo-Lin@users.noreply.github.com>
2024-10-13 04:44:08 +0800

    Time: 0 ms (100.00%), Space: 12.2 MB (45.35%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -15,7 +15,7 @@ public:
         if(!p && !q){
             return true;
         }
-        if(!p && q || p && !q || p->val!=q->val){
+        if((!p && q) || (p && !q) || (p->val!=q->val)){
             return false;
         }
         return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
