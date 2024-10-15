eb96b617926102e92249ddf4437f859ac8a43db0
Elmo Lin <75987103+Elmo-Lin@users.noreply.github.com>
2024-10-16 07:23:51 +0800

    Time: 17 ms (36.28%), Space: 27 MB (67.03%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -3,30 +3,30 @@ public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        int res=0;
         int count;
+        int res=0;
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(grid[i][j]==1){
                     count=0;
-                    dfs(grid, i, j, count, res);
+                    dfs(grid, res, i, j, count);
                 }
             }
         }
         return res;
     }
-    void dfs(vector<vector<int>>& grid, int i, int j, int& count, int& res){
+    void dfs(vector<vector<int>>& grid, int& res, int i, int j, int& count){
         int m=grid.size();
         int n=grid[0].size();
-        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]!=1){
+        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]==0){
             return;
         }
         count++;
         res=max(res, count);
         grid[i][j]=0;
-        dfs(grid, i+1, j, count, res);
-        dfs(grid, i, j+1, count, res);
-        dfs(grid, i-1, j, count, res);
-        dfs(grid, i, j-1, count, res);
+        dfs(grid, res, i+1, j, count);
+        dfs(grid, res, i, j+1, count);
+        dfs(grid, res, i-1, j, count);
+        dfs(grid, res, i, j-1, count);
     }
 };
