7cd3165844b1a453ee6a432363c6289a606bea0d
Elmo Lin <75987103+Elmo-Lin@users.noreply.github.com>
2024-11-03 23:01:40 +0800

    Time: 6 ms (63.29%), Space: 12.7 MB (75.32%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -15,7 +15,7 @@ public:
                 if(word1[i-1]==word2[j-1]){
                     dp[i][j]=dp[i-1][j-1];
                 }else{
-                    dp[i][j]=min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1]))+1;
+                    dp[i][j]=min(dp[i-1][j], min(dp[i][j-1], dp[i-1][j-1]))+1;
                 }
             }
         }
